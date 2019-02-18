1ecb446914610d7be25d367a6d5b41e7be21bb29
Daniele Decaro <danieledecaro113@gmail.com>
2019-02-19 00:17:47 +0100

    wrong vnf files

--- vnfs/webclient/Dockerfile
+++ vnfs/webclient/Dockerfile
@@ -1,5 +1,9 @@
-FROM glanf/base:latest
-	
+FROM node:latest
+
+RUN apt-get update && apt-get install -y \
+	bridge-utils \
+	net-tools \
+    inetutils-traceroute
 ADD run.sh /tmp/run.sh
 RUN chmod +x /tmp/run.sh
 ENTRYPOINT ["/tmp/run.sh"]
--- vnfs/webclient/run.sh
+++ vnfs/webclient/run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo $DROP
-IFS=';' read -ra ADDR <<< "$DROP"
-for i in "${ADDR[@]}"; do
-    # $i has rule in form src,dest
-    IFS=', ' read -ra SRCDST <<< "$i"
-    iptables -A FORWARD -s "${SRCDST[0]}" -d "${SRCDST[1]}" -j REJECT
-done
+ip route add "$(getent hosts "$nameWebServer" | awk '{ print $1 }')" via "$(getent hosts "$nextHop" | awk '{ print $1 }')"
 while true; do sleep 15 ; done
